dae4382b869402ff39edb9d498a0af351e66cc9c
Ben Healey <bentastic27@gmail.com>
2022-04-21 15:25:17 -0400

    init of terraform stuff

---
+++ terraform/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    rafay = {
+      version = ">= 0.1"
+      source  = "RafaySystems/rafay"
+    }
+  }
+}
+
+provider "rafay" {
+  provider_config_file = var.rafay_config_file
+}
+
+resource "rafay_eks_cluster" "ekscluster" {
+  cluster {
+    kind = "Cluster"
+    metadata {
+      name    = var.name
+      project = var.project
+      labels = var.labels
+    }
+    spec {
+      type           = "eks"
+      blueprint      = "default"
+      blueprint_version = "1.12.0"
+      cloud_provider = var.cluster.cloud_credentials
+      cni_provider   = var.cluster.cni_provider
+      proxy_config   = {}
+    }
+  }
+  cluster_config {
+    apiversion = "rafay.io/v1alpha5"
+    kind       = "ClusterConfig"
+    metadata {
+      name    = var.name
+      region  = var.cluster.region
+      version = var.cluster.version
+    }
+    vpc {
+      cidr = var.cluster.cidr
+      cluster_endpoints {
+        private_access = var.cluster.private_access
+        public_access  = var.cluster.public_access
+      }
+      nat {
+        gateway = "Single"
+      }
+    }
+    node_groups {
+      name       = var.node_group.name
+      ami_family = var.node_group.ami_family
+      iam {
+        iam_node_group_with_addon_policies {
+          image_builder = true
+          auto_scaler   = true
+        }
+      }
+      ssh {
+        allow = var.node_group.ssh_allow
+        enable_ssm = true
+        public_key_name = var.node_group.ssh_public_key_name
+      }
+      instance_name = var.node_group.instance_name
+      instance_type    = var.node_group.instance_type
+      desired_capacity = var.node_group.desired_capacity
+      min_size         = var.node_group.min_size
+      max_size         = var.node_group.max_size
+      max_pods_per_node = 50
+      version          = "1.21"
+      volume_size      = 50
+      volume_type      = "gp3"
+      private_networking = true
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,66 @@
+variable "name" {
+  type = string
+  default = "eks-cluster"
+}
+
+variable labels {
+  type = map(string)
+  default = {}
+}
+
+variable "project" {
+  type = string
+  default = "defaultproject"
+}
+
+variable "rafay_config_file" {
+  type = string
+  default = "~/.rafay/cli/config.json"
+  
+}
+
+variable "cluster" {
+  type = object({
+    region = string
+    version = string
+    cidr = string
+    cni_provider = string
+    cloud_credentials = string
+    private_access = bool
+    public_access = bool
+  })
+  default = {
+    region = "us-west-2"
+    cidr = "192.168.0.0/16"
+    cloud_credentials = "some-credentials"
+    cni_provider = "aws-cni"
+    private_access = true
+    public_access = false
+    version = "1.21"
+  }
+}
+
+variable "node_group" {
+  type = object({
+    name = string
+    ami_family = string
+    instance_name = string
+    instance_type = string
+    ssh_allow = bool
+    ssh_public_key_name = string
+    min_size = number
+    max_size = number
+    desired_capacity = number
+  })
+  default = {
+    ami_family = "AmazonLinux2"
+    name = "ng-1"
+    instance_name = "terraform-eks-instance"
+    instance_type = "t2.large"
+    ssh_allow = false
+    ssh_public_key_name = ""
+    min_size = 1
+    max_size = 2
+    desired_capacity = 1
+  }
+}
