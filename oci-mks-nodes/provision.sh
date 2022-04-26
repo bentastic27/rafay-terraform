f088585972d39e3588988214d9d7e20ae384587d
Ben Healey <bentastic27@gmail.com>
2022-04-26 14:29:06 -0400

    change cluster name based on tfvars

--- oci-mks-nodes/provision.sh
+++ oci-mks-nodes/provision.sh
@@ -4,6 +4,9 @@ terraform apply
 
 cp cluster-base.yaml cluster.yaml
 
+CLUSTER_NAME=$(grep cluster_name terraform.tfvars | awk '{print $3}'| cut -f2 -d\")
+sed -i "s/some-cluster/${CLUSTER_NAME}/g" cluster.yaml
+
 for masterip in $(terraform show -json | jq -r '.values.root_module.resources | .[] | select(.name == "master_instance") | .values.display_name + "," + .values.public_ip')
 do
   echo "    - hostname: $(echo $masterip | cut -f1 -d,)" >> cluster.yaml
