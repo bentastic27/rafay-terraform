4cc8551d3263e1ed8894737940f98a1c7f2548e8
Ben Healey <bentastic27@gmail.com>
2022-11-07 11:42:38 -0500

    Added SSH outputs for nodes

--- ec2-kubeadm-import/output.tf
+++ ec2-kubeadm-import/output.tf
@@ -1,3 +1,15 @@
 output "kubectl" {
   value = var.install_kubernetes ? "kubectl --kubeconfig ${path.module}/ansible-output/kubeconfig.yaml get nodes" : null
 }
+
+output "master_ssh" {
+  value = [
+    for instance in aws_instance.kubeadm_master : "ssh ${var.instance_ami_user}@${instance.public_ip} -i ${var.instance_keypair_file}"
+  ]
+}
+
+output "worker_ssh" {
+  value = [
+    for instance in aws_instance.kubeadm_worker : "ssh ${var.instance_ami_user}@${instance.public_ip} -i ${var.instance_keypair_file}"
+  ]
+}
